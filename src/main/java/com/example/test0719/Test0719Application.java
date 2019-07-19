55b975ea766a9f23e6cf73028da6bb2ca17a9abd
ElaineMis <1350396534@qq.com>
2019-07-19 08:51:52 +0800

    搭建项目环境

---
+++ src/main/java/com/example/test0719/Test0719Application.java
@@ -0,0 +1,13 @@
+package com.example.test0719;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0719Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0719Application.class, args);
+    }
+
+}
